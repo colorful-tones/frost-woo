bc2243642644ca660c6e188d3f46556c071730f3
Damon Cook <damonacook@gmail.com>
2022-07-27 19:08:03 -0400

    Revert "👌 IMPROVE: font colors in collection-cover.php"
    This reverts commit 4133c7aff589ef57cae6b1737fc5559506794869.

--- patterns/collection-cover.php
+++ patterns/collection-cover.php
@@ -10,17 +10,17 @@
 <!-- wp:cover {"url":"<?php
 echo esc_url( get_theme_file_uri( 'assets/images/front-page/cesar-la-rosa-HbAddptme1Q-unsplash.jpg' ) ); ?>","id":43,"dimRatio":30,"minHeight":726,"minHeightUnit":"px","customGradient":"linear-gradient(135deg,rgb(6,147,227) 0%,rgb(228,13,13) 71%,rgb(155,81,224) 100%)","isDark":false} -->
 <div class="wp-block-cover is-light" style="min-height:726px" id="featured"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-30 has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(135deg,rgb(6,147,227) 0%,rgb(228,13,13) 71%,rgb(155,81,224) 100%)"></span><img class="wp-block-cover__image-background wp-image-43" alt="" src="<?php
-echo esc_url( get_theme_file_uri( 'assets/images/front-page/cesar-la-rosa-HbAddptme1Q-unsplash.jpg' ) ); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1"}},"textColor":"base","fontSize":"small"} -->
-<p class="has-text-align-center has-base-color has-text-color has-small-font-size" style="line-height:1">FALL COLLECTION</p>
+echo esc_url( get_theme_file_uri( 'assets/images/front-page/cesar-la-rosa-HbAddptme1Q-unsplash.jpg' ) ); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1"}},"textColor":"background","fontSize":"small"} -->
+<p class="has-text-align-center has-background-color has-text-color has-small-font-size" style="line-height:1">FALL COLLECTION</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0px","bottom":"1.5rem"}}},"textColor":"base","fontSize":"max-60"} -->
-<h2 class="has-text-align-center has-base-color has-text-color has-max-60-font-size" style="margin-top:0px;margin-bottom:1.5rem">MEGA LOUNGE</h2>
+<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0px","bottom":"1.5rem"}}},"textColor":"background","fontSize":"max-60"} -->
+<h2 class="has-text-align-center has-background-color has-text-color has-max-60-font-size" style="margin-top:0px;margin-bottom:1.5rem">MEGA LOUNGE</h2>
 <!-- /wp:heading -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"textColor":"base","className":"is-style-outline"} -->
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-base-color has-text-color wp-element-button">SHOP NOW</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"background","className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-background-color has-text-color wp-element-button">SHOP NOW</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div></div>
 <!-- /wp:cover -->
