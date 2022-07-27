386577f338e68ed160ff1ed5f9a54fa39e592a98
Damon Cook <damonacook@gmail.com>
2022-07-27 15:18:27 -0400

    ➕ ADD: updated header-default and footer-default

---
+++ patterns/header-default.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Header with site title, navigation.
+ * Slug: frost/header-default
+ * Categories: frost-header
+ * Block Types: core/template-part/header
+ * Viewport Width: 1280
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px"},"margin":{"top":"0px"}}},"layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:30px;padding-bottom:30px"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-title /-->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"layout":{"type":"flex","orientation":"horizontal"},"style":{"spacing":{"blockGap":"20px"}}} -->
+<!-- wp:page-list /-->
+<!-- /wp:navigation -->
+
+<!-- wp:woocommerce/mini-cart {"addToCartBehaviour":"open_drawer","textColor":"primary","fontFamily":"heading","fontSize":"small"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
